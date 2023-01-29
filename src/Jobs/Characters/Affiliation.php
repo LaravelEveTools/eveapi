29f11f734dd0cc65f02a66cbbcab0f91906b1443
Lgullestrup <lgullestrup@gmail.com>
2023-01-29 21:18:28 +1000

    fixed affilitation job to accept multiple character ids

--- src/Jobs/Characters/Affiliation.php
+++ src/Jobs/Characters/Affiliation.php
@@ -3,14 +3,24 @@
 namespace LaravelEveTools\EveApi\Jobs\Characters;
 
 use LaravelEveTools\EveApi\Jobs\Abstracts\AbstractCharacterJob;
+use LaravelEveTools\EveApi\Jobs\Abstracts\EsiBase;
 
 /**
  * Bulk lookup of character IDs to corporation, alliance and faction
  * 
  * https://esi.evetech.net/ui/#/Character/post_characters_affiliation
  */
-abstract class Affiliation extends AbstractCharacterJob
+abstract class Affiliation extends EsiBase
 {
+    const REQUEST_ID_LIMIT = 1000;
+    /**
+     * @var array
+     */
+    protected $character_ids;
+
+    public function __construct(array $character_ids){
+        $this->character_ids = $character_ids;
+    }
 
     protected $method = 'post';
 
@@ -20,4 +30,16 @@ abstract class Affiliation extends AbstractCharacterJob
 
     protected $tags = ['character', 'public'];
 
+        /**
+     * {@inheritdoc}
+     */
+    public function tags(): array
+    {
+        $tags = parent::tags();
+
+        if(! in_array('character', $tags))
+            $tags[] = 'character';
+
+        return $tags;
+    }
 }
